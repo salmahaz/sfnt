d0b43a8d12c05c35c57fb8d25b399bf3c0128dcf
Salma <salmahazimeh2018@gmail.com>
2025-01-16 03:15:01 -0800

    Initial commit before running codemod

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Footer(){
+
+    
+    return (
+        <div className="bg-gray-400 w-full fixed bottom-0 text-white py-5 text-center">
+            <p>Bysage Lazzaro, Fortune, 5 Sep. 2023</p>
+        </div>
+    )
+}
